d4acbfab54aa3b25f44e207bd26ea5c6c474295d
SeemaAggarwal <121787754+DkAggarwal2512@users.noreply.github.com>
2024-06-04 15:36:24 +0530

    Add files via upload

---
+++ Buffer.java
@@ -0,0 +1,26 @@
+
+import java.io.*;
+
+
+public class Buffer {
+    public static void main(String[] args) throws IOException {
+       BufferedReader a= new BufferedReader(new InputStreamReader(System.in));
+       System.out.println("Enter your age: ");
+       String name = a.readLine();
+       System.out.println("Your name is :"+ name);
+       
+
+
+
+
+    //    java.io.BufferedReader a = new java.io.BufferedReader(new InputStreamReader(System.in));
+       
+       
+       
+       
+       
+       
+       
+    }
+    
+}
